098f11ba32cc1fc51a9a8e062f038572b0ea4432
zz41354899 <81232904+zz41354899@users.noreply.github.com>
2025-11-24 17:29:53 +0800

    feat: Integrate Tailwind CSS for styling and consolidate global styles into `src/index.css`.

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,25 @@
 import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig(() => {
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      },
-      define: {
-        'process.env': {},
+  return {
+    server: {
+      port: 3000,
+      host: '0.0.0.0',
+    },
+    plugins: [
+      react(),
+      tailwindcss(),
+    ],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
       }
-    };
+    },
+    define: {
+      'process.env': {},
+    }
+  };
 });
